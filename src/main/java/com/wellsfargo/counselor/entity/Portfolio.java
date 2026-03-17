fa791c51a0dbc6b60f6e89037ca1dfd8ae574ef6
Aryan <nandvilkararyan@gmail.com>
2026-03-17 21:59:40 +0530

    Add Client, Portfolio, and Security entities

---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,42 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue()
+    private long portfolioId;
+
+    @ManyToOne
+    @JoinColumn(name = "clientId", nullable = false)
+    private Client client;
+
+    @Column(nullable = false)
+    private LocalDate creationDate;
+
+    @OneToMany(mappedBy = "portfolio")
+    private List<Security> securities;
+
+    protected Portfolio() {}
+
+    public Portfolio(Client client, LocalDate creationDate, List<Security> securities) {
+        this.client = client;
+        this.creationDate = creationDate;
+        this.securities = securities;
+    }
+
+    public Long getPortfolioId() { return portfolioId; }
+
+    public Client getClient() { return client; }
+    public void setClient(Client client) { this.client = client; }
+
+    public LocalDate getCreationDate() { return creationDate; }
+    public void setCreationDate(LocalDate creationDate) { this.creationDate = creationDate; }
+
+    public List<Security> getSecurities() { return securities; }
+    public void setSecurities(List<Security> securities) { this.securities = securities; }
+}
